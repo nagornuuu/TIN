7f1980dfb723e3e5cf45fd663046a51af5f75569
Andrii Nahornyi <nagornyiandriy@gmail.com>
2023-11-12 22:44:35 +0100

    file adding

--- Node.js - wprowadzenie/Zadanie 5.js
+++ Node.js - wprowadzenie/Zadanie 5.js
@@ -1,34 +1,41 @@
-const http = require('http');
-const url = require('url');
+// module http i url
+const http = require("http");
+const url = require("url");
 
+// Server
 const server = http.createServer((req, res) => {
-    const queryObject = url.parse(req.url, true).query;
-    const { x, y } = queryObject;
+
+    // Pobiernie argumentów z adresu URL
+    const { x, y } = url.parse(req.url, true).query;
     const operation = url.parse(req.url, true).pathname.substring(1);
 
-    let result;
-    switch (operation) {
-        case 'dodaj':
-            result = parseFloat(x) + parseFloat(y);
-            break;
-        case 'odejmij':
-            result = parseFloat(x) - parseFloat(y);
-            break;
-        case 'pomnóż':
-            result = parseFloat(x) * parseFloat(y);
-            break;
-        case 'podziel':
-            result = parseFloat(x) / parseFloat(y);
-            break;
-        default:
-            result = 'Invalid operation';
+    // sprawdzenie czy argumenty są liczbami
+    if (isNaN(parseFloat(x)) || isNaN(parseFloat(y))) {
+        return res.end('<h1>Argumenty nie sa liczbami</h1>');
     }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`<html><body>Wynik operacji ${operation} dla liczb ${x} i ${y} to: ${result}</body></html>`);
+    // operacje matematyczne
+    let result;
+    if (operation === 'dodaj') {
+        result = parseFloat(x) + parseFloat(y);
+    } else if (operation === 'odejmij') {
+        result = parseFloat(x) - parseFloat(y);
+    } else if (operation === 'pomnoz') {
+        result = parseFloat(x) * parseFloat(y);
+    } else if (operation === 'podziel') {
+        if (parseFloat(y) === 0) {
+            return res.end('<h1>Nie mozna dzielic przez zero</h1>');
+        }
+        result = parseFloat(x) / parseFloat(y);
+    } else {
+        return res.end('<h1>Nieprawidlowa operacja</h1>');
+    }
+
+    // Wynik w postaci minimalistycznej strony HTML
+    res.end(`<h1>${x} ${operation} ${y} = ${result}</h1>`, 'utf-8');
 });
 
 const port = 3000;
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`http://localhost:${port}/`);
 });
